96a0927d5ad8d23642faade93a4471bca1e5caae
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-26 23:33:25 +0530

    --screen setup done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="app">
-      <h1>Instagram Reels Clone</h1>
       <div className="app-top">
         {/* image */}
+        <img
+          className="app-logo"
+          src={logo}
+          alt="instagram-logo">
+        </img>
         {/* reels text */}
+        <h1>Reels</h1>
       </div>
       <div className="app-videos">
         {/* Scrollable container for videos */}
+        {/* video */}
+        {/* video */}
+        {/* video */}
+        {/* video */}
       </div>
     </div>
   );
