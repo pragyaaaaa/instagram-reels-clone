8a5842b68bf767e5915564b3f261040f1f6c26b2
pragyaaaaa <pragyasabstracts@gmail.com>
2020-08-27 01:10:01 +0530

    --UI complete.

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './VideoFooter.css'
 import { Button, Avatar } from '@material-ui/core';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ModeCommentIcon from '@material-ui/icons/ModeComment';
+import SendIcon from '@material-ui/icons/Send';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import Ticker from 'react-ticker';
 function VideoFooter({ url, likes, shares, channel, avatarSrc, song }) {
     return (
@@ -23,6 +27,24 @@ function VideoFooter({ url, likes, shares, channel, avatarSrc, song }) {
                             </>
                         )}
                     </Ticker>
+                </div>
+                <div className="video-footer-action">
+                    <div className="video-footer-action-left">
+                        <FavoriteIcon fontSize="large"></FavoriteIcon>
+                        <ModeCommentIcon fontSize="large"></ModeCommentIcon>
+                        <SendIcon fontSize="large"></SendIcon>
+                        <MoreHorizIcon fontSize="large"></MoreHorizIcon>
+                    </div>
+                    <div className="video-footer-action-right">
+                        <div className="video-footer-stat">
+                            <FavoriteIcon></FavoriteIcon>
+                            <p>{likes}</p>
+                        </div>
+                        <div className="video-footer-stat">
+                            <ModeCommentIcon></ModeCommentIcon>
+                            <p>{shares}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
